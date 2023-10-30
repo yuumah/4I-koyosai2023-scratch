fc1601beaf9b336ca25d7f1b2c1439d2a2220f4c
kaikaikaito <kaikai.mot@gmail.com>
2023-10-30 15:39:44 +0900

    10/30作業分

--- content.js
+++ content.js
@@ -7,7 +7,8 @@ document.onkeydown=e=>{
       html.classList.toggle("color");return false;
     }
   }
-  return html.classList.contains("color") || (/Numpad[0-9]|Digit[0-9]/.test(e.code)&&!e.altKey&&!e.ctrlKey&&!e.shiftKey);
+  return html.classList.contains("color") || (!/F[1-9][0-9]?/.test(e.code)&&!e.altKey&&!e.ctrlKey&&!e.shiftKey);
+  //return html.classList.contains("color") || (/Numpad[0-9]|Digit[0-9]|NumpadSubtract|Minus/.test(e.code)&&!e.altKey&&!e.ctrlKey&&!e.shiftKey);
 }
 
 document.oncontextmenu=e=>{
@@ -17,3 +18,21 @@ document.oncontextmenu=e=>{
 
 // ポップアップでやる
 // window.open("https://scratch.mit.edu/projects/editor/","_blank","scrollbars=yes,resizable=yes,fullscreen=yes")
+
+// const NG_Label=[];
+// const NG_dataid=[];
+// const NG_
+
+// const canvas_elements = document.querySelectorAll(".blocklyFlyout .blocklyBlockCanvas > *");
+// let blocks = {};
+// let tmp = [];
+// canvas_elements.forEach(elem=>{
+//   tmp.push(elem);
+//   if(elem.classList.contains("blocklyFlyoutLabel") || elem.classList.contains("blocklyDraggable") || elem.classList.contains("blocklyFlyoutButton")){
+//     blocks[elem.getAttribute("transform").match(/translate\(\d+,(\d+)\)/)[1]] = tmp;
+//     tmp = [];
+//   }
+// })
+
+// let keys=Object.keys(blocks)
+// keys.sort((x,y)=>x-y)
